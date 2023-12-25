ad9b12b3449c3fe4260e5e5e30ef05aea45d8d93
Elisheva Aizen <154802992+ElishevaAizen@users.noreply.github.com>
2023-12-25 18:31:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from rest_api_client import RestAPIClient
 def main():
   url="https://resttest10.herokuapp.com"
   
-  #create an instace for rest_api_client object
+  #create an instace for RestAPIClient
   rest_api_client=RestAPIClient(url)
   
   ####Task_1####
@@ -15,7 +15,7 @@ def main():
   print(response_2)
   
   ####Task_2####
-  #send the responses for posting request
+  #send posting request with the responses
   response=rest_api_client.send_request(response_1,response_2)
   #check if posting is done successfully
   assert(response.ok)
